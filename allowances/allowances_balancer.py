d0e4d10c54e1545bc2b3b0770a01cf0da11b1b14
nledo <nicolas@karpatkey.com>
2023-09-07 16:51:23 -0300

    Balancer db / allowances_balancer.py / Curve db update

--- allowances/allowances_balancer.py
+++ allowances/allowances_balancer.py
@@ -13,6 +13,7 @@ MTA = "0xa3BeD4E1c75D00fa6f4E5E6922DB7261B5E9AcD2"
 D2D = "0x43D4A3cd90ddD2F8f4f693170C9c8098163502ad"
 COMP = "0xc00e94Cb662C3520282E6f5717214004A7f26888"
 AAVE = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9"
+rETH = "0xae78736Cd615f374D3085123A210448E74Fc6393"
 rETH2 = "0x20BC832ca081b91433ff6c17f85701B6e92486c5"
 SWISE = "0x48e5413b73add2434e47504E2a22d14940dBFe78"
 sETH2 = "0xFe2e637202056d30016725477c5da089Ab0A043A"
@@ -21,6 +22,8 @@ USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
+stETH = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
+wstETH = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
 
 # Across V2 contracts
 ACROSS_HUB_POOL_V2 = "0xc186fA914353c44b2E33eBE05f21846F1048bEda"
@@ -40,60 +43,84 @@ BALANCER_VAULT = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
 # Cowswap contracts
 GPv2_VAULT_RELAYER = "0xC92E8bdf79f0507f65a392b0ab4667716BFE0110"
 
+# Compound V3 contracts
+cUSDCv3 = "0xc3d688B66703497DAA19211EEdff47f25384cdc3"
+
+# Rocket Pool contracts
+ROCKET_SWAP_ROUTER = "0x16D5A408e807db8eF7c578279BEeEe6b228f1c1C"
+
+# Maker contracts
+MAKER_DSR_MANAGER = "0x373238337Bfe1146fb49989fc222523f83081dDb"
+
+# Lido contracts
+unstETH = "0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1"
+
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # LITERALS
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ALLOWANCES = [
     {
-        "token": BAL,
+        "token": stETH,
+        "spender": wstETH
+    },
+    {
+        "token": stETH,
+        "spender": unstETH
+    },
+    {
+        "token": wstETH,
+        "spender": unstETH
+    },
+    {
+        "token": USDC,
+        "spender": cUSDCv3
+    },
+    {
+        "token": USDC,
         "spender": AAVE_POOL_V3
     },
     {
-        "token": BAL,
-        "spender": ACROSS_HUB_POOL_V2
+        "token": DAI,
+        "spender": AAVE_POOL_V3
     },
     {
-        "token": BAL,
-        "spender": SILO_ROUTER_V2
+        "token": rETH,
+        "spender": ROCKET_SWAP_ROUTER
     },
     {
-        "token": MTA,
-        "spender": stkMTA
+        "token": DAI,
+        "spender": MAKER_DSR_MANAGER
     },
-    {
-        "token": BAL,
-        "spender": BALANCER_VAULT
-    },
-    {
-        "token": D2D,
-        "spender": BALANCER_VAULT
-    },
-    {
-        "token": COMP,
-        "spender": GPv2_VAULT_RELAYER
-    },
+    # {
+    #     "token": BAL,
+    #     "spender": AAVE_POOL_V3
+    # },
+    # {
+    #     "token": BAL,
+    #     "spender": ACROSS_HUB_POOL_V2
+    # },
+    # {
+    #     "token": BAL,
+    #     "spender": SILO_ROUTER_V2
+    # },
+    # {
+    #     "token": MTA,
+    #     "spender": stkMTA
+    # },
+    # {
+    #     "token": BAL,
+    #     "spender": BALANCER_VAULT
+    # },
+    # {
+    #     "token": D2D,
+    #     "spender": BALANCER_VAULT
+    # },
     {
         "token": AAVE,
         "spender": GPv2_VAULT_RELAYER
     },
     {
-        "token": rETH2,
-        "spender": GPv2_VAULT_RELAYER
-    },
-    {
-        "token": SWISE,
-        "spender": GPv2_VAULT_RELAYER
-    },
-    {
-        "token": sETH2,
-        "spender": GPv2_VAULT_RELAYER
-    },
-    {
-        "token": WETH,
-        "spender": GPv2_VAULT_RELAYER
-    },
-    {
-        "token": USDC,
+        "token": COMP,
         "spender": GPv2_VAULT_RELAYER
     },
     {
@@ -101,11 +128,44 @@ ALLOWANCES = [
         "spender": GPv2_VAULT_RELAYER
     },
     {
+        "token": rETH,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
+        "token": rETH2,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
+        "token": sETH2,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
+        "token": stETH,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
+        "token": SWISE,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
+        "token": USDC,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    
+    {
         "token": USDT,
         "spender": GPv2_VAULT_RELAYER
     },
     {
+        "token": WETH,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
         "token": WBTC,
+        "spender": GPv2_VAULT_RELAYER
+    },
+    {
+        "token": wstETH,
         "spender": GPv2_VAULT_RELAYER
     }
 ]
